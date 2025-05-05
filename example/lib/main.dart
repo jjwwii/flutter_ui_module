369e1d016bbcb67b2ec77007f44eee3bacf23bf7
wonil <dnjsdlfdld@gmail.com>
2025-05-05 17:28:02 +0900

    1. first commit
    - flutter_ui_module

---
+++ example/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_module/extensions/context_extension.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Text('Flutter Demo Home Page'),
+    );
+  }
+}
+
